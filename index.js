26085fcf77d443b02ab512b5718645d0f317f2b2
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-08-06 22:29:50 +0530

    Random tag functionality

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
+
+  if (e.key === "Enter") {
+    setTimeout(() => {
+      e.target.value = "";
+    }, 10);
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -22,4 +29,39 @@ function createTags(input) {
 
     tagsEl.appendChild(tagEL);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+      //   alert("Picked Option is ", randomTag.innerText);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
