4e1381ea6b7da9f402a973df07e8f9d36f778f9e
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-08-06 22:19:17 +0530

    Added Css and JS for creating tags

---
+++ index.js
@@ -0,0 +1,25 @@
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEL = document.createElement("span");
+    tagEL.classList.add("tag");
+    tagEL.innerText = tag;
+
+    tagsEl.appendChild(tagEL);
+  });
+}
